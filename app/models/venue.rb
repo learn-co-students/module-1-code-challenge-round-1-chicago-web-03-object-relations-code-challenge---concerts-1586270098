79ebff880d42509b3785e64543748201471084db
imperfectcastle <bart.sakowski@gmail.com>
2020-04-07 10:58:16 -0500

    modified:   app/models/band.rb 	modified:   app/models/concert.rb 	modified:   app/models/venue.rb 	modified:   tools/console.rb

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -12,4 +12,34 @@ class Venue
     def self.all
         @@all
     end
+
+    def concerts
+        #return array of all concerts for this venue
+        Concert.all.find_all do |show|
+            show.venue == self
+        end
+    end
+
+    def bands
+        #return array of all bands for venue
+        self.concerts.map do |show|
+            show.band
+        end
+    end
+
+    def concert_on(date)
+        self.concerts.find do |show|
+            show.date == date
+        end
+    end
+
+    def most_frequent_band(band)
+        # returns most frequently band
+        # i need to go in and return the most frequently played band. 
+        self.concerts.max_by { |show| self.concerts.count(show)}
+
+
+    end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+lawrence_arms = Band.new("The Lawrence Arms", "Chicago")
+veil_of_maya = Band.new("Veil of Maya", "Milwaukee")
+alkaline_trio = Band.new("Alkaline Trio", "Chicago")
+maybeshewill = Band.new("Maybeshewill", "Newcastle")
+
+
+msg = Venue.new("Madison Square Garden", "New York")
+bowery_ballroom = Venue.new("Bowery Ballrom", "New York")
+reggies = Venue.new("Reggie's", "Chicago")
+bottom_lounge = Venue.new("Bottome Lounge", "Chicago")
+cobra_lounge = Venue.new("Cobra Lounge", "Chicago")
+
+may_show = Concert.new("May 8", maybeshewill, reggies)
+april_show = Concert.new("April 20", maybeshewill, cobra_lounge)
+summer_show = Concert.new("July 5", veil_of_maya, reggies)
+hometown_show = Concert.new("August 3", alkaline_trio, bottom_lounge)
+may_show_two = Concert.new("May 8", lawrence_arms, reggies)
+larry_show = Concert.new("June 6" ,lawrence_arms, reggies)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
