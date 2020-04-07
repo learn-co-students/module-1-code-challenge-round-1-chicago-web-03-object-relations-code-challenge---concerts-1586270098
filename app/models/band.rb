54c9f1b09dc66bbf6d60b856acd77a143cc5ddfc
Claudiuts <sarb.claudiu391@gmail.com>
2020-04-07 10:57:56 -0500

    not complete, need to learn better time managment

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,13 +3,29 @@ class Band
     attr_reader :hometown
     @@all = []
 
-    def initialize(name, hometown)
-        @name = name
-        @hometown = hometown
-        @@all << self
-    end
+        def initialize(name, hometown)
+            @name = name
+            @hometown = hometown
+            @@all << self
+        end
 
-    def self.all
-        @@all
-    end
+        def self.all
+            @@all
+        end
+
+        def self.band_name(name)
+            @@all.find{|band| band.name == name}
+        end
+
+        def self.band_hometown(hometown)
+            self.all.find do |hometown|
+                if band.hometown == band_hometown
+                    p hometown
+                end
+            end
+        end
+
+        def all_bands
+            @@all << Band.new(name)
+        end
 end
