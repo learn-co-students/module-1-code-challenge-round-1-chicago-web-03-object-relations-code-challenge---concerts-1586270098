e80dfa4f244050102ff84920a7204886b77b5b80
Johnathan <johnathanguzman17@gmail.com>
2020-04-07 10:57:22 -0500

    code challenge

--- app/models/band.rb
+++ app/models/band.rb
@@ -6,10 +6,25 @@ class Band
     def initialize(name, hometown)
         @name = name
         @hometown = hometown
-        @@all << self
+        @@all.push(self)
     end
 
     def self.all
         @@all
     end
+
+    def name(name)
+        self.name = name
+        return name
+    end
+
+    def hometown
+        @hometown
+    end
+
+    # return concert when called?
+
+    # def band
+    #     @concert
+    # end
 end
