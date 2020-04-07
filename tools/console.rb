d878efdc1e8c4d3d0bc92f2e83513e7a6e90a7f3
jogamer130 <jogamer130@gmail.com>
2020-04-07 10:57:48 -0500

    Was not able to finish, but worked until I ran out of time

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,12 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+the_rookies = Band.new("The Rookies", "Crown Point")
+
+super_strong = Venue.new("Super Strong Music Strom", "Chicago")
+
+greatest_rookies = Concert.new("Saturday", the_rookies, super_strong)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
